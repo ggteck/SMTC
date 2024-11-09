ac91daba60456564c44ea9a237b3e02de23ea5af
GilRuiz84 <56622853+GilRuiz84@users.noreply.github.com>
2024-11-09 16:23:53 -0600

    V3. Descarga de .bat para jupyterhub

---
+++ scripts/jupyter.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Checking if JupyterLab is installed...
+
+REM Check if JupyterLab is available
+jupyter-lab --version >nul 2>&1
+
+REM If the last command (jupyter-lab --version) failed, install JupyterLab
+if errorlevel 1 (
+    echo JupyterLab is not installed. Installing now...
+    pip install jupyterlab
+    echo JupyterLab has been installed.
+)
+
+echo Starting JupyterLab in the specified directory...
+
+REM Change the directory to your specified folder
+cd /D "%~1"
+
+REM Start JupyterLab
+call jupyter-lab
+
+echo JupyterLab has started.
+pause
